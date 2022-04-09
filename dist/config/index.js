ca00660ae3cf2f22e9b25d576b5e0bb3e4e3c370
Juan Carlos Cruz <juancarloscruz278@gmail.com>
2022-04-08 23:44:55 -0400

    refactor: removes dist files

--- dist/config/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwtSecret: 'my_secret_key',
-    db: 'mongodb+srv://jc:1234@cluster0.o17ou.mongodb.net/rest-api-typescript?retryWrites=true&w=majority',
-};
